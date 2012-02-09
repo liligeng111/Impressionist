1e6d2f5286a6b546fc9bebed8530b833b5dc12f4
WANG HENG <genencn@gmail.com>
2012-02-10 01:11:32 +0800

    trivial modification, several comments added

---
+++ impressionist.h
@@ -0,0 +1,20 @@
+#ifndef IMPRESSIONIST_H
+#define IMPRESSIONIST_H
+#define SAMPLE_SOLUTION
+#include <stdio.h>
+
+#include <FL/Fl.H>
+#include <FL/gl.h>
+#include <GL/glu.h>
+
+#include "impbrush.h"
+
+#define TRUE	1
+#define FALSE	0
+
+#ifdef WIN32
+#define M_PI	3.1415926535F
+#endif
+
+
+#endif
